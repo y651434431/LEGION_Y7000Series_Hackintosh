454c94b2bbb946ee88eea7f2f239bd6e4c665861
xiaoMGitHub <zxmyhd@163.com>
2019-07-17 23:57:14 +0800

    Update 安装完成后执行.sh

--- Script/安装完成后执行.sh
+++ Script/安装完成后执行.sh
@@ -5,6 +5,6 @@ sudo pmset -a hibernatemode 0
 sudo rm /var/vm/sleepimage
 sudo mkdir /var/vm/sleepimage
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/xiaoMGithub/LocalTime-Toggle/master/fix_time_osx.sh)"
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/xiaoMGithub/Lenovo_Y7000-Y530_Hackintosh/master/ALCPlugFix/alc_fix.sh)"
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/xiaoMGithub/Lenovo_Y7000-Y530_Hackintosh/master/Script/alc_fix.sh)"
 echo "执行完成，请重启"
 exit 0
